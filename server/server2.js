9a07340454ca856be9996017c30d62470c2628ab
fremen432 <cbmiller4897@gmail.com>
2022-02-03 21:02:18 -0600

    duplicated server.js

--- server/server2.js
+++ server/server2.js
@@ -9,13 +9,21 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers,
-  context: authMiddleware
-});
+const startServer = async () => {
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context: authMiddleware
+  });
 
-server.applyMiddleware({ app });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -32,6 +40,5 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
