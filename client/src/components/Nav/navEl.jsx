9e4d0d6e0d94469ed95e0a0c1119a1e3ed1204de
fremen432 <cbmiller4897@gmail.com>
2022-02-08 16:40:45 -0600

    signup and logout works

--- client/src/components/Nav/navEl.jsx
+++ client/src/components/Nav/navEl.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
-class NavEl extends React.Component {
+import Auth from '../../utils/auth'
 
-  navElements =  [
+class NavEl extends Component {
+
+  logout(event) {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  loggedOutNavElements =  [
     { name: 'Home',             ref: '/', },
     { name: 'Products',         ref: 'products', },
     { name: 'About',            ref: 'about', },
@@ -14,6 +21,17 @@ class NavEl extends React.Component {
     { name: 'Log In',           ref: 'login', },
     { name: 'Sign Up',          ref: 'signup', },
   ]
+  loggedInNavElements =  [
+    { name: 'Home',             ref: '/', },
+    { name: 'Products',         ref: 'products', },
+    { name: 'About',            ref: 'about', },
+    { name: 'Contact',          ref: 'contact', },
+    { name: 'Admin',            ref: 'admin', },
+    { name: 'Cart',             ref: 'cart', },
+    { name: 'Order History',    ref: 'orderHistory', },
+    { name: 'Order Summary',    ref: 'orderSummary', },
+    { name: 'Log Out',          ref: '/', onClick: this.logout}
+  ]
   basicClasses = ` nav-el inline-flex  items-center px-1  pt-1 text-sm  font-medium  border-b-2`
   nonFocusedClasses = ` text-gray-500 border-transparent hover:border-gray-300 hover:text-gray-700`
   focusedClasses = ` text-gray-900 border-indigo-500 `
@@ -21,16 +39,33 @@ class NavEl extends React.Component {
   render() {
     return (
       <>
-        {
-          this.navElements.map( el => 
-            <a  
-              href={ el.ref } 
-              key={ el.ref } 
-              onClick={ this.changeClass } 
-              className={ this.basicClasses + this.nonFocusedClasses } 
-            >{ el.name }</a>
-          )
-        }
+        {Auth.loggedIn() ? (
+          <>
+            {
+              this.loggedInNavElements.map( el => 
+                <a  
+                  href={ el.ref } 
+                  key={ el.name } 
+                  onClick={ el.onClick } 
+                  className={ this.basicClasses + this.nonFocusedClasses } 
+                >{ el.name }</a>
+              )
+            }
+          </>
+        ) : (
+          <>
+            {
+              this.loggedOutNavElements.map( el => 
+                <a  
+                  href={ el.ref } 
+                  key={ el.name } 
+                  onClick={ el.onClick } 
+                  className={ this.basicClasses + this.nonFocusedClasses } 
+                >{ el.name }</a>
+              )
+            }
+          </>
+        )}
       </>
     )
   }
