82c184e9237ca0a717352361b45813ee9dfd16d3
fremen432 <cbmiller4897@gmail.com>
2022-04-13 01:59:42 -0500

    NavEl

--- client/src/newComponents/Header/Header.jsx
+++ client/src/newComponents/Header/Header.jsx
@@ -4,7 +4,7 @@ import { SearchIcon } from '@heroicons/react/solid'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 // Importing components
-import NavEl from './NavEl.jsx'
+import NavEl from './NavEl'
 import Logo from './logo'
 import HamburgerIcon from './HamburugerIcon'
 
