37c613f18bd42b8ff0c311129fb71d93ce73152c
fremen432 <cbmiller4897@gmail.com>
2022-04-14 16:11:17 -0500

    updated server gitignore

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// import express from 'express';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -10,7 +11,13 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://burkholder_eyelashes:<password>@cluster0.bfmpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MongoDB_UserName1 = 'burkholder_eyelashes'
+const MongoDB_Password1 = 'Hannah38'
+
+const MongoDB_UserName2 = 'burkholder_eyelashes_user1'
+const MongoDB_Password2 = '1iQsMDRr4dk3BGhe'
+
+const CONNECTION_URL = `mongodb+srv://${MongoDB_UserName1}:${MongoDB_Password1}@cluster0.bfmpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTechnology: true })
