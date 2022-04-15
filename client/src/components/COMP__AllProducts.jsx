37d185f2a7f4bf864347231981cf8b37576a6f93
fremen432 <cbmiller4897@gmail.com>
2022-04-15 09:34:35 -0500

    cleaned up COMP_AllProducts.jsx. each product now has a key

--- client/src/components/COMP__AllProducts.jsx
+++ client/src/components/COMP__AllProducts.jsx
@@ -1,69 +1,54 @@
 import React, { useState } from 'react'
-import { products } from "../data"
+// import { products } from "../data"
 
 import { useSelector } from 'react-redux';
 
 const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
 
 function ProductCard(props){
-  const { product, page, onAdd, onRemove } = props;
+  const { product, page, onAdd, index } = props;
 
-
+  product.id = index;
   return(
     <div key={product.id} className="productCard m-1 box">
-    {/* Product Image */}
+      {/* Product Image */}
       <img
         src={product.imageSrc}
         alt={product.imageAlt}
         className="w-full h-full object-center object-cover bg-gray-200 rounded-md overflow-hidden lg:h-72 xl:h-80"
       />
       {/* Product Description */}
-    <div className=" flex justify-between flex-col ">
+      <div className=" flex justify-between flex-col ">
 
-      <div className="productCard-innerWrapper  ">
-        <h3 className="mt-2 text-sm text-gray-700 box text-center">
+        <div className="productCard-innerWrapper  ">
+          <h3 className="mt-2 text-sm text-gray-700 box text-center">
+          
+            <a href={product.href}>
+              <span className="absolute " />
+              {product.name}
+            </a>
+          </h3>
+
+          <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+          {/* <p className="productDescription" >{product.description}</p> */}
+          <p className="productDescription text-xs p-1 mt-1 text-sm text-gray-500" >{description}</p>
+        </div>
         
-          <a href={product.href}>
-            <span className="absolute " />
-            {product.name}
-          </a>
-        </h3>
-
-        <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-        {/* <p className="productDescription" >{product.description}</p> */}
-        <p className="productDescription text-xs p-1 mt-1 text-sm text-gray-500" >{description}</p>
+        <div className={"grid mb-2 " + (page == 'allProducts' ? 'grid-cols-2 ' : 'grid-cols-1 ')}>
+          <button onClick={() => onAdd(product)} className={"productCard-checkoutBtn p-1 text-sm font-medium text-gray-900 box rounded-lg" + (page == 'allProducts' ? ' ' : ' hidden')}>Add to Cart</button>
+          <p className=" flex justify-end items-center text-sm font-medium text-gray-900 text-right box">{product.price}</p>
+        </div>
+        
       </div>
-      
-      <div className={"grid mb-2 " + (page == 'allProducts' ? 'grid-cols-2 ' : 'grid-cols-1 ')}>
-        <button onClick={() => onAdd(product)} className={"productCard-checkoutBtn p-1 text-sm font-medium text-gray-900 box rounded-lg" + (page == 'allProducts' ? ' ' : ' hidden')}>Add to Cart</button>
-        <p className=" flex justify-end items-center text-sm font-medium text-gray-900 text-right box">{product.price}</p>
-      </div>
-      
-    </div>
   </div>
 )
 }
 
-// function ProductCards(props) {
-//     const { page, onAdd, onRemove, key, setKey } = props;
-//     console.log(page)
-
-//     return (
-//         <>
-//           {/* Create a product card for each product in products array */}
-//           {products.map((product, index) => {
-//               product.id = index;
-//               return <ProductCard key={index} product={product} page={page} onAdd={onAdd} onRemove={onRemove} />
-//           })}
-//       </>
-//     )
-//   }
-
 export default function AllProducts(props) {
 
     // the state.products comes from reducers index.js
-    // const products = useSelector((state) => state.products)
-    // console.log(products);
+    const products = useSelector((state) => state.products)
+    console.log(products);
 
     const { page, onAdd, onRemove } = props;
     return (
@@ -75,12 +60,9 @@ export default function AllProducts(props) {
             {/* Products container */}
             <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-10 sm:gap-x-6 sm:grid-cols-2 lg:grid-cols-4  lg:gap-y-0 lg:gap-x-8">
 
-                {products.map((product, index) => {
-                    product.id = index;
-                    return <ProductCard key={index} product={product} page={page} onAdd={onAdd} onRemove={onRemove} />
-                })}
-
-                {/* <ProductCards key={key} page={page} onAdd={onAdd} onRemove={onRemove} /> */}
+              {products.map((product, index) => 
+                <ProductCard key={index} product={product} page={page} onAdd={onAdd} index={index} />
+              )}
 
             </div>
 
