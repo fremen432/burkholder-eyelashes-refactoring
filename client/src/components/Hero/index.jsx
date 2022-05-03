2121162aa273ee9ace35975123ec52e3412fa638
fremen432 <cbmiller4897@gmail.com>
2022-05-03 14:28:21 -0500

    COMPONENT__Hero styles done

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -8,14 +8,14 @@ export default function Hero(props) {
     const handleClick = () => dispatch(setPageMethods.allProducts());
 
     return (
-        <div id="Hero" className="section SECTION_Hero topBottom_section relative bg-gray-900">
+        <div id="Hero" className="COMPONENT__Hero ">
             {/* Hero Image */}
             <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
-            <img
-                src="https://cdn.shopify.com/s/files/1/0539/7102/6115/files/hero_img_2x_1101db36-e570-49ae-ab38-455ea9f412b1_1728x.png?v=1629399562s"
-                alt=""
-                className="w-full h-full object-center object-cover"
-            />
+                <img
+                    src="https://cdn.shopify.com/s/files/1/0539/7102/6115/files/hero_img_2x_1101db36-e570-49ae-ab38-455ea9f412b1_1728x.png?v=1629399562s"
+                    alt=""
+                    className="w-full h-full object-center object-cover"
+                />
             </div>
 
             {/* Dimming layer */}
